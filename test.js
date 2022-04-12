db8f9624b47552748dc9930081a5e24094a3d5e0
ChanKengHup <dungk51a2@gmail.com>
2022-04-12 10:54:20 +0700

    new

--- test.js
+++ test.js
@@ -1,9 +1,2 @@
 console.log("test 1");
-<<<<<<< HEAD
 console.log("thay đổi");
-
-console.log("revert 1");
-console.log("revert 2");
-=======
-console.log("thay đổi");
->>>>>>> parent of 06bf774 (revert 1)
